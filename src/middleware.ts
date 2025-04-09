bc2ea91ac7dcd0132fedbbf6bce63467114cdfcd
vaka.dev <vaka.coding@gmail.com>
2025-04-09 20:41:57 +0700

    membuat arcjet-shield dan protectBot di middlaware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-import { createRouteMatcher } from "@clerk/nextjs/server";
+import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
@@ -7,21 +8,36 @@ const isProtectedRoute = createRouteMatcher([
   "/transactions(.*)",
 ]);
 
-// Middleware untuk protect routes jika belum login
-export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth();
+/// Create Arcjet middleware
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!, // ✅ tambahkan tanda seru agar dipastikan tidak undefined
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
+    }),
+  ],
+});
+
+// Middleware dari Clerk
+const clerk = clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
 
   if (!userId && isProtectedRoute(req)) {
-    const { redirectToSignIn } = await auth();
     return redirectToSignIn();
   }
-}); 
 
+  return NextResponse.next();
+});
+
+// Gabungkan Arcjet dan Clerk middleware
+export default createMiddleware(aj, clerk);
+
+// Config untuk middleware agar match route yang diinginkan
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
